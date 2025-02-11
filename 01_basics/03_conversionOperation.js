be4e68976e4469b35cef159ebc708a8b7542cf83
ArhamRasheed <arhamrasheed20@gmail.com>
2025-02-11 11:20:46 +0000

    convertionOperations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+//let score = "33abc"
+// let score = null
+// let score = undefined
+// let score = true
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNUmber = Number(score)
+// console.log(valueInNUmber); // output: NaN not a number
+// console.log(typeof (valueInNUmber));
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+// let isloggedIn = 1 => true
+// let isloggedIn = "" => false
+// let isloggedIn = "Arham" => true
+let isloggedIn = -25
+let booleanIsloggedIn = Boolean(isloggedIn)
+//console.log(booleanIsloggedIn)
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log( typeof stringNumber)
+
+// Browser zda tar string values deta h, apko int, boolean, array, and objects mn conver krna porta h 
