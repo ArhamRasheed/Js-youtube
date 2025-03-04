b335cd5cd33d0739ee1ebd4bec9863443a0cbef0
ArhamRasheed <arhamrasheed20@gmail.com>
2025-03-04 02:51:43 +0000

    strings

---
+++ 01_basics/06_Strings.js
@@ -0,0 +1,10 @@
+const name = "hitesh "
+const repoCount = 50
+
+//THIS SYNTAX IS NOT RECOMMENDED
+//console.log(name + repoCount + " Value"); 
+
+//backticks => strings interpolation
+console.log(`Hello my name is ${name} and my repo count is ${repoCount} `)
+
+const gameName = new String('ArhamRasheed')
