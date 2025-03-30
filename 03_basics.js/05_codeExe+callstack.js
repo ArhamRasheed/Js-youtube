7e1d1e0b7e18efcbff86304d9815b006313b0978
ArhamRasheed <arhamrasheed20@gmail.com>
2025-03-30 12:49:49 +0000

    code stack

---
+++ 03_basics.js/05_codeExe+callstack.js
@@ -0,0 +1,10 @@
+// humra code global execution context mn hota h 
+// ye pura ek single thread mn execute hota h 
+// javascript is single threaded har chez ek process mn execute hoti h 
+
+/*
+Execution Context:
+1)Global Execution Context
+2)Function Execution Context
+3)Eval Execution Context => apne ap mn ek global object ki property hota h 
+*/
